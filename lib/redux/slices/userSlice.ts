9d49fa5415e49f29a325e668e6e5e2daf046999e
Nitin Gupta <gupta7nitin@gmail.com>
2025-11-29 18:58:50 +0530

    feat: Create userSlice for managing user state in Redux with actions for setting, updating, and clearing user data

---
+++ lib/redux/slices/userSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface UserState {
+  id: string | null
+  name: string | null
+  email: string | null
+  image: string | null
+  phoneNumber: string | null
+  countryCode: string | null
+  bio: string | null
+}
+
+const initialState: UserState = {
+  id: null,
+  name: null,
+  email: null,
+  image: null,
+  phoneNumber: null,
+  countryCode: null,
+  bio: null,
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<Partial<UserState>>) => {
+      return { ...state, ...action.payload }
+    },
+    updateUserProfile: (state, action: PayloadAction<Partial<UserState>>) => {
+      return { ...state, ...action.payload }
+    },
+    clearUser: () => initialState,
+  },
+})
+
+export const { setUser, updateUserProfile, clearUser } = userSlice.actions
+export default userSlice.reducer
