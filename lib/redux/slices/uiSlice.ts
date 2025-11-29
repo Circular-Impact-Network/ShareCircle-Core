a704b952c83c27ee02e459708178e78bd9400100
Nitin Gupta <gupta7nitin@gmail.com>
2025-11-29 18:58:56 +0530

    feat: Add uiSlice for managing UI state in Redux with actions for toggling mobile sidebar and setting theme

---
+++ lib/redux/slices/uiSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface UIState {
+  isMobileSidebarOpen: boolean
+  theme: 'light' | 'dark'
+}
+
+const initialState: UIState = {
+  isMobileSidebarOpen: false,
+  theme: 'light',
+}
+
+export const uiSlice = createSlice({
+  name: 'ui',
+  initialState,
+  reducers: {
+    toggleMobileSidebar: (state) => {
+      state.isMobileSidebarOpen = !state.isMobileSidebarOpen
+    },
+    setMobileSidebarOpen: (state, action: PayloadAction<boolean>) => {
+      state.isMobileSidebarOpen = action.payload
+    },
+    setTheme: (state, action: PayloadAction<'light' | 'dark'>) => {
+      state.theme = action.payload
+    },
+  },
+})
+
+export const { toggleMobileSidebar, setMobileSidebarOpen, setTheme } = uiSlice.actions
+export default uiSlice.reducer
