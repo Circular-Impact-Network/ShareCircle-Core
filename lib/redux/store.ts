d3e869c0dff24e2de043ffda6c3301bbf25b0993
Nitin Gupta <gupta7nitin@gmail.com>
2026-01-31 10:00:00 +0530

    feat(redux): integrate notifications, borrow, and messages APIs into store

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -3,6 +3,9 @@ import userReducer from './slices/userSlice';
 import uiReducer from './slices/uiSlice';
 import { userApi } from './api/userApi';
 import { itemsApi } from './api/itemsApi';
+import { borrowApi } from './api/borrowApi';
+import { notificationsApi } from './api/notificationsApi';
+import { messagesApi } from './api/messagesApi';
 
 export const store = configureStore({
 	reducer: {
@@ -10,8 +13,18 @@ export const store = configureStore({
 		ui: uiReducer,
 		[userApi.reducerPath]: userApi.reducer,
 		[itemsApi.reducerPath]: itemsApi.reducer,
+		[borrowApi.reducerPath]: borrowApi.reducer,
+		[notificationsApi.reducerPath]: notificationsApi.reducer,
+		[messagesApi.reducerPath]: messagesApi.reducer,
 	},
-	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware, itemsApi.middleware),
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(
+			userApi.middleware,
+			itemsApi.middleware,
+			borrowApi.middleware,
+			notificationsApi.middleware,
+			messagesApi.middleware
+		),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
