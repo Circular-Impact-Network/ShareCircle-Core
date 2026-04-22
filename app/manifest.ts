2395a8918b0037dea1e2b8f30f908d5c19cdb886
Nitin Gupta <gupta7nitin@gmail.com>
2026-04-23 00:18:28 +0530

    chore: updated PWA icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,25 +16,25 @@ export default function manifest(): MetadataRoute.Manifest {
 		lang: 'en',
 		icons: [
 			{
-				src: '/share-circle-logo-no-name.png',
+				src: '/icons/icon-192.png',
 				sizes: '192x192',
 				type: 'image/png',
 				purpose: 'any',
 			},
 			{
-				src: '/share-circle-logo-no-name.png',
+				src: '/icons/icon-512.png',
 				sizes: '512x512',
 				type: 'image/png',
 				purpose: 'any',
 			},
 			{
-				src: '/share-circle-logo-no-name.png',
+				src: '/icons/icon-192.png',
 				sizes: '192x192',
 				type: 'image/png',
 				purpose: 'maskable',
 			},
 			{
-				src: '/share-circle-logo-no-name.png',
+				src: '/icons/icon-512.png',
 				sizes: '512x512',
 				type: 'image/png',
 				purpose: 'maskable',
